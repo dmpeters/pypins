25018095fc9e34f12bc165e26802ebd5a05d0062
dmpeters <dmpeters63@gmail.com>
2012-03-10 21:13:44 -0800

    init integration of app - pypins.py

--- app/pypins.py
+++ app/pypins.py
@@ -37,5 +37,10 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run()
