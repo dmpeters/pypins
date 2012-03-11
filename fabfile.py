dedec3b73547ac2debc801c4cb3e272b920473cc
Adam Venturella <aventurella@gmail.com>
2012-03-10 20:57:34 -0800

    create db

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,12 @@
 from fabric.api import env
 from fabric.api import run
+from fabric.api import local
 from fabric.api import task
 
+@task 
+def create_database():
+    local("sqlite3 pypins.sqlite < schema.sql")
+
 @task
-def mytask():
+def populate_data():
     pass
